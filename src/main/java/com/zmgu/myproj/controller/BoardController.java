f2bf31c442f32fe512370556279737e6c2db6271
zmgu <122958434+zmgu@users.noreply.github.com>
2023-09-16 22:12:03 +0900

    BoardList

--- src/main/java/com/zmgu/myproj/controller/BoardController.java
+++ src/main/java/com/zmgu/myproj/controller/BoardController.java
@@ -1,15 +1,28 @@
 package com.zmgu.myproj.controller;
 
+import com.zmgu.myproj.domain.Board;
+import com.zmgu.myproj.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired
+    private BoardRepository boardRepository;
+
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards", boards);
+
         return "board/list";
     }
 }
---
+++ src/main/java/com/zmgu/myproj/domain/Board.java
@@ -0,0 +1,18 @@
+package com.zmgu.myproj.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+}

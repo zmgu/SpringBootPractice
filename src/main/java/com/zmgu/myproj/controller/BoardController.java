85bd134eeb1c56337daff232f4a68cef79eddec8
zmgu <122958434+zmgu@users.noreply.github.com>
2023-09-18 01:06:09 +0900

    form

--- src/main/java/com/zmgu/myproj/controller/BoardController.java
+++ src/main/java/com/zmgu/myproj/controller/BoardController.java
@@ -2,11 +2,14 @@ package com.zmgu.myproj.controller;
 
 import com.zmgu.myproj.domain.Board;
 import com.zmgu.myproj.repository.BoardRepository;
+import com.zmgu.myproj.validator.BoardValidator;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +20,9 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
 
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
     public String list(Model model) {
 
@@ -25,4 +31,27 @@ public class BoardController {
 
         return "board/list";
     }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if(id == null) {
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@Valid Board board, BindingResult bindResult) {
+        boardValidator.validate(board, bindResult);
+
+        if(bindResult.hasErrors()){
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
+    }
+
 }
--- src/main/java/com/zmgu/myproj/domain/Board.java
+++ src/main/java/com/zmgu/myproj/domain/Board.java
@@ -4,7 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.NonNull;
 
 @Entity
 @Data
@@ -13,6 +16,8 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min= 2, max = 30, message = "제목은 2자 이상 30자 이하입니다.")
     private String title;
     private String content;
 }

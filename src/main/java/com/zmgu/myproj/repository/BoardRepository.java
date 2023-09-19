cf90d7edfa2965c6be3317dd6cedbc540097c708
zmgu <122958434+zmgu@users.noreply.github.com>
2023-09-19 11:46:07 +0900

    RESTful API 작성

--- src/main/java/com/zmgu/myproj/repository/BoardRepository.java
+++ src/main/java/com/zmgu/myproj/repository/BoardRepository.java
@@ -3,5 +3,10 @@ package com.zmgu.myproj.repository;
 import com.zmgu.myproj.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
 }

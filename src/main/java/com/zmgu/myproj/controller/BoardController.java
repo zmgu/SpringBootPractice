626ea35726d9b3649e02fdcdde9b9ad415710f12
zmgu <122958434+zmgu@users.noreply.github.com>
2023-09-21 00:18:29 +0900

    paging & search

--- src/main/java/com/zmgu/myproj/controller/BoardController.java
+++ src/main/java/com/zmgu/myproj/controller/BoardController.java
@@ -5,7 +5,9 @@ import com.zmgu.myproj.repository.BoardRepository;
 import com.zmgu.myproj.validator.BoardValidator;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.bind.BindResult;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,9 +26,15 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model) {
+    public String list(Model model, @PageableDefault(size = 10) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText) {
+//        Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+        int startPage = Math.max(1,boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
 
-        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
 
         return "board/list";
--- src/main/java/com/zmgu/myproj/repository/BoardRepository.java
+++ src/main/java/com/zmgu/myproj/repository/BoardRepository.java
@@ -1,6 +1,8 @@
 package com.zmgu.myproj.repository;
 
 import com.zmgu.myproj.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +11,6 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     List<Board> findByTitle(String title);
     List<Board> findByTitleOrContent(String title, String content);
+
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }
